236a8045af8db6b346f1543bddf3e46ae236db71
HTV-LongTH <lonth@hybrid-technologies.co.jp>
2020-02-18 16:22:48 +0700

    add source

--- src/main/java/info/cafeit/springbeandefinationoverriding/SpringBeanDefinationOverridingApplication.java
+++ src/main/java/info/cafeit/springbeandefinationoverriding/SpringBeanDefinationOverridingApplication.java
@@ -1,13 +1,19 @@
 package info.cafeit.springbeandefinationoverriding;
 
 import info.cafeit.springbeandefinationoverriding.bean.Actor;
+import info.cafeit.springbeandefinationoverriding.configuration.ScenesConfig;
+import info.cafeit.springbeandefinationoverriding.configuration.ScenesConfig2;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
+@ComponentScan(excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = ScenesConfig.class))
 public class SpringBeanDefinationOverridingApplication implements CommandLineRunner {
 
     public static void main(String[] args) {
--- src/main/java/info/cafeit/springbeandefinationoverriding/configuration/ScenesConfig.java
+++ src/main/java/info/cafeit/springbeandefinationoverriding/configuration/ScenesConfig.java
@@ -3,13 +3,12 @@ package info.cafeit.springbeandefinationoverriding.configuration;
 import info.cafeit.springbeandefinationoverriding.bean.Actor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
+
 public class ScenesConfig {
 
     @Bean
-    @Primary
     public Actor actresses() {
         return new Actor("Maria Ozawa");
     }
